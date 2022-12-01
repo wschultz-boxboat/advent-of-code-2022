1fc8117dbdf79a615ece83352d04e1f335ec3739
Will Schultz <will.schultz@ibm.com>
2022-12-01 13:07:33 -0600

    completion of puzzle 01 for day 01

---
+++ 01/puzzle-01/go.mod
@@ -0,0 +1,3 @@
+module github.com/advent-of-code-2022/01/puzzle-01
+
+go 1.19
---
+++ 01/puzzle-01/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fileData, err := os.ReadFile("./input.txt")
+	checkError(err)
+
+	maxCalories := countCalories(string(fileData))
+
+	fmt.Println(maxCalories)
+
+}
+
+func checkError(e error) {
+	if e != nil {
+		fmt.Println("hit error")
+		panic(e)
+	}
+}
+
+func countCalories(data string) int {
+	elves := getAllElvesCalories(data)
+	elvesTotalCalories := sumElvesCalories(elves)
+	maxCalories := getHighestCalorieElf(elvesTotalCalories)
+
+	return maxCalories
+
+}
+
+func getAllElvesCalories(data string) map[int][]int {
+	var elves = make(map[int][]int)
+
+	values := strings.Split(data, "\n")
+
+	index := 0
+
+	for _, value := range values {
+		if value == "" {
+			index++
+			continue
+		}
+
+		valueInt, err := strconv.Atoi(value)
+		checkError(err)
+
+		elves[index] = append(elves[index], valueInt)
+	}
+
+	return elves
+}
+
+func sumElvesCalories(elves map[int][]int) map[int]int {
+	var summedElves = make(map[int]int)
+
+	for elf, values := range elves {
+		totalCalories := 0
+		for _, calories := range values {
+			totalCalories += calories
+		}
+
+		summedElves[elf] = totalCalories
+	}
+
+	return summedElves
+}
+
+func getHighestCalorieElf(elves map[int]int) int {
+	maxCalories := 0
+
+	for _, calories := range elves {
+		if calories > maxCalories {
+			maxCalories = calories
+		}
+	}
+
+	return maxCalories
+}
